9960570cdc781bb152fa1c51a48f851bc5866ddb
Erin Kahn <erinnicolekahn@gmail.com>
2018-06-15 14:56:24 -0500

    burger done!

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,2 +1,10 @@
-"use strict";console.log("Hello World from app.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+console.log(`Hello World from app.js! 
+Change this message, and make sure it changes in the browser 
+to verify that you're working in the right files.`);
+
+document.querySelector('.burger').addEventListener('click', function () {
+    //show menu
+    document.querySelector('.burger').classList.toggle('open');
+    document.querySelector('.nav').classList.toggle('open');
+});
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
 console.log(`Hello World from app.js! 
 Change this message, and make sure it changes in the browser 
 to verify that you're working in the right files.`)
+
+
+
+document.querySelector('.burger').addEventListener('click', function(){
+    //show menu
+    document.querySelector('.burger').classList.toggle('open');
+    document.querySelector('.nav').classList.toggle('open');
+})
