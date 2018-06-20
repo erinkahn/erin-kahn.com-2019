3d764a2bf430ddf5b200ff5ea452556fb32dd74e
Erin Kahn <erinnicolekahn@gmail.com>
2018-06-20 12:44:12 -0400

    popUp content loop - need help

--- dist/js/main.js
+++ dist/js/main.js
@@ -11,7 +11,17 @@ let popUp = document.querySelectorAll('.popUp');
 
 for (let i = 0; i < popUp.length; i++) {
     popUp[i].addEventListener('click', function () {
+        //loop through popup content
+        let content = document.querySelectorAll('.content');
         document.querySelector('.portfolio-popUp').classList.add('show');
     });
-}
+};
+
+// if class is show and you click X
+// hide portfoio popUp
+let x = document.querySelector('.x');
+x.addEventListener('click', function () {
+    console.log('x was clicked');
+    document.querySelector('.portfolio-popUp').classList.remove('show');
+});
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -14,6 +14,35 @@ let popUp = document.querySelectorAll('.popUp');
 
 for (let i=0; i< popUp.length; i++){
     popUp[i].addEventListener('click', function() {
+        //loop through popup content
+        let content = document.querySelectorAll('.content');
         document.querySelector('.portfolio-popUp').classList.add('show');
-    })
-}
+
+        // if the pop up is showing then loop through the content and show it in the popup box
+        if (document.querySelector('.portfolio-popUp').style.display ==== 'block'){
+            console.log('the class of show is on');
+            //loop through content individually 
+            for (let j=0;  j<content.length; j++){
+                //show content one by one based on which project you click
+                content[i].classList.add('contentShow');
+            } 
+        else {
+            content[i].classList.remove('contentShow');
+
+        }
+    })  
+};
+
+
+
+
+
+
+
+// if class is show and you click X
+// hide portfoio popUp
+let x = document.querySelector('.x');
+x.addEventListener('click', function() {
+    console.log('x was clicked');
+    document.querySelector('.portfolio-popUp').classList.remove('show');
+});
