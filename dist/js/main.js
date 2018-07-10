a5c135485eb8013bad4656246c10526bb682d29b
Erin Kahn <erinnicolekahn@gmail.com>
2018-07-10 10:32:44 -0400

    added dim bkg behind popup

--- dist/js/main.js
+++ dist/js/main.js
@@ -20,8 +20,10 @@ for (let i = 0; i < popUp.length; i++) {
     popUp[i].addEventListener('click', function () {
         console.log('you clicked a project' + i); // not showing in console
 
-        //shop the popup box
+        //show the popup box
         document.querySelector('.portfolio-popUp').classList.add('show');
+        // add a dimmed background behind popup
+        document.body.classList.add('dim-bkg');
 
         // loop through the content 
         // hide all other content except for the one thats clicked
@@ -34,15 +36,78 @@ for (let i = 0; i < popUp.length; i++) {
     });
 };
 
-// popup x button close ---------------------------------------------------------------
+// close the popUp box ---------------------------------------------------------------
 
 // if class is show and you click X
 // hide portfoio popUp
 let x = document.querySelector('.x');
+let popUpBox = document.querySelector('.portfolio-popUp');
+
 x.addEventListener('click', function () {
     console.log('x was clicked');
-    document.querySelector('.portfolio-popUp').classList.remove('show');
+    popUpBox.classList.remove('show');
+    document.body.classList.remove('dim-bkg');
 });
+
+// var bodyTag = document.getElementsByTagName('body');
+// var element = document.querySelector('.portfolio-popUp'); 
+// function clickedOrNot(e) {
+// 	if (e.target !== element) {
+// 		// action in the case of click outside 
+// 		bodyTag[0].removeEventListener('click', clickedOrNot, true);
+// 	}	
+// }
+// bodyTag[0].addEventListener('click', clickedOrNot, true);
+
+
+// Detect all clicks on the document
+// document.addEventListener("click", function(event) {
+
+//     // If user clicks inside the element, do nothing
+//     if (event.target.closest(".portfolio-popUp")) return;
+
+//     // If user clicks outside the element, hide it!
+//         box.classList.add("js-is-hidden");
+
+// });
+
+
+// document.addEventListener("click", function(evt) {  
+//     var popUpBox = document.querySelector('.portfolio-popUp'),
+//         targetElement = evt.target;  // clicked element
+
+//     do {
+//         if (targetElement == popUpBox) {
+//             // This is a click inside. Do nothing, just return.
+//             console.log("Clicked inside!");
+//             return;
+//         }
+//         // Go up the DOM
+//         targetElement = targetElement.parentNode;
+//     } while (targetElement);
+
+//     // This is a click outside.
+//     console.log("Clicked outside!");
+// });
+
+
+// let bigbox = document.querySelector('.portfolio-popUp')
+
+// $('#showbox').click(function(){
+
+//     $('#bigbox').show(function(){
+//       document.body.addEventListener('click', boxCloser, false);
+//     });
+
+//   });
+
+// function boxCloser(e){
+//     if(e.target.id != 'bigbox'){
+//        document.body.removeEventListener('click', boxCloser, false);
+//        $('.portfolio-popUp').hide();
+//     }
+// }
+
 
 //  hero arrow down scroll----------------------------------------------------------
 
